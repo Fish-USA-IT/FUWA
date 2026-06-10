57e3d4544f37394957458daa1e150c2dc59ab0b7
Joshua McMahan <joshua@fishusa.com>
2026-06-10 16:22:42 -0400

    Changes for Cloud Build

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@ using NetSuite.SchemaSdk.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"https://0.0.0.0:{port}");
+
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddSession(options =>
